6d7b58d757c15eaddc9d132de4b61752afa83161
108charlotte <152224795+108charlotte@users.noreply.github.com>
2025-05-24 11:10:16 +0000

    started trying to get webpack to work but im having issues with imports and things

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -8,11 +9,31 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        clean: true,
     }, 
     devServer: {
         static: './dist',
         port: 3000,
         open: true
     },
-
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+            filename: 'index.html'
+        })
+    ],
 }
