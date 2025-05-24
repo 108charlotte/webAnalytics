7cc9ca05e551555329c7a7f935baa2b02f9f3319
108charlotte <152224795+108charlotte@users.noreply.github.com>
2025-05-24 10:58:14 +0000

    set up test deployment with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        bundle: './src/index.js'
+    }, 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].js',
     }, 
+    devServer: {
+        static: './dist',
+        port: 3000,
+        open: true
+    },
+
 }
